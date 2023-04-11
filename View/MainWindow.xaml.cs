3f78d3183f1d0285a5e43f2409812c874802f7ac
deofi <deofi@DESKTOP-E5PHOB4>
2023-04-11 07:01:11 +0300

    Добавьте файлы проекта.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using RVP.Task5.Core;
+using System.Windows;
+using RVP.Task5.View.TaskPages;
+
+
+namespace RVP.Task5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+             InitializeComponent();
+            
+        }
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+      
+        private void BtnTask1_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage1());
+        }
+        private void BtnTask2_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage2());
+        }
+        private void BtnTask3_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage3());
+        }
+        private void BtnTask4_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage4());
+        }
+        private void BtnTask5_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage5());
+        }
+        private void BtnTask6_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage6());
+        }
+        private void BtnTask7_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage7());
+        }
+        private void BtnTask8_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage8());
+        }
+        private void BtnTask9_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage9());
+        }
+       
+        private void BtnTask10_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage10());
+        }
+        private void BtnTask11_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage11());
+        }
+        private void BtnTask12_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage12());
+        }
+        private void BtnTask13_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new TaskPage13());
+        }
+    }
+}
